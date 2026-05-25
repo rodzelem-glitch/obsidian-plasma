76847124836727a4331ea456f69c17e168097caf
Roderick Macdonell <rodzelem@gmail.com>
2026-05-25 09:54:21 -0500

    fix(android): enable edge-to-edge in MainActivity for SDK 35/36 Play Console compatibility

--- TekTrakker-v2/android/app/src/main/java/com/tektrakker/MainActivity.java
+++ TekTrakker-v2/android/app/src/main/java/com/tektrakker/MainActivity.java
@@ -1,10 +1,17 @@
 package com.tektrakker;
 
 import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
 import com.getcapacitor.BridgeActivity;
 import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
 
 public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
+        super.onCreate(savedInstanceState);
+    }
     
     @Override
     public void IHaveModifiedTheMainActivityForTheUseWithSocialLoginPlugin() {
